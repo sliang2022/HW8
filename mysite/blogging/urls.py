2882f93272fe175fd0337fed5ba9f585ecc01f3f
sliang2022 <118237216+sliang2022@users.noreply.github.com>
2023-12-03 20:33:27 -0800

    Update urls.py

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -3,6 +3,5 @@ from blogging.views import list_view, detail_view
 
 urlpatterns = [
     path('posts/', list_view.as_view(), name="blog_index"),
-    #path('posts/stub_view', stub_view, name="stub_view"),
-    path('posts/<int:post_id>/', detail_view.as_view(), name="blog_detail"),
+    path('posts/<int:pk>/', detail_view.as_view(), name="blog_detail"),
 ]
